2ebda23fad41f6200a5a88f08c8e2c9e85f88f8b
knevins1 <katharine.nevins@gmail.com>
2025-02-15 19:30:17 -0500

    Create EstateplannerApp.js

---
+++ components/EstateplannerApp.js
@@ -0,0 +1,9 @@
+import EstateplannerApp from '../components/EstateplannerApp'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen">
+      <EstateplannerApp />
+    </main>
+  )
+}
